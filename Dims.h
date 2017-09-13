c80486c83b41d3841d7ae6c37f0305485991bbe9
shadow_walker <shadowwalkersb@gmail.com>
2017-09-12 22:52:58 -0500

    latest from dims_v1/

--- Dims.h
+++ Dims.h
@@ -2,3 +2,29 @@
 #include <iostream>
 
 using namespace std;
+
+template <int D, class T>
+class Dim {
+  public:
+    Dim()
+	    : vals(D, T())
+      {}
+    
+    Dim(T v)
+	    : val(v)
+      {}
+    
+    vector<T> vals;
+    T val;
+};
+
+template <int D, class T>
+ostream& operator<<(ostream& out, const Dim<D,T> &obj){
+  for(typename vector<T>::const_iterator it=obj.vals.begin();
+		  it!=obj.vals.end();
+		  ++it
+     )
+    out<<" "<<*it;
+  
+  return out;
+}
