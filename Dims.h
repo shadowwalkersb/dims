9c3d85e66cfd3516753755b90640ad8c34b74c53
shadow_walker <shadowwalkersb@gmail.com>
2017-09-12 22:52:58 -0500

    classes: Dim, Dim<1,T>

--- Dims.h
+++ Dims.h
@@ -2,3 +2,24 @@
 #include <iostream>
 
 using namespace std;
+
+template <int D, class T>
+class Dim {
+  public:
+    Dim()    : dim(T()), dims()  {}
+    Dim(T v) : dim(v),   dims(v-1) {
+    }
+
+    T dim;
+    Dim<D-1,T> dims;
+};
+
+template <class T>
+class Dim<1,T> {
+  public:
+    Dim()    : dim(T()) {}
+    Dim(T v) : dim(v)   {
+    }
+
+    T dim;
+};
